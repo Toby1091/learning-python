ffd9a73f1f58f9b952b6871df80b77db85b237a2
Toby1091 <112862755+Toby1091@users.noreply.github.com>
2022-10-18 20:58:50 +0200

    initial commit

---
+++ Practice.py
@@ -0,0 +1,21 @@
+# Clean code book
+# Github
+# https://docs.python.org/3/library/index.html
+# https://docs.python.org/3/tutorial/index.html
+# list comprehension
+# context
+# udemy tutorial
+# official python documentation
+# Library Reference
+# data type "set"
+# regular expression
+# Unicode vs. codec
+# iterators
+# itertools
+# pathlib — Object-oriented filesystem paths
+# os.path — Common pathname manipulations
+# glob
+# shutil
+# sqlite3 — DB-API 2.0 interface for SQLite databases
+# json — JSON encoder and decoder
+# https://peps.python.org/pep-0008/
