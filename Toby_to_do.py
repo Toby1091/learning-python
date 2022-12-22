b410466a482f81e64bbcd89ee145a4d07ed8d1a7
Tobias Arbogast <tobias.arbogast@gmail.com>
2022-12-22 18:00:38 +0000

    Create ToDo list of learning goals

---
+++ Toby_to_do.py
@@ -0,0 +1,43 @@
+# TODO:
+# compute total # of speeches speech_analysis (cb_count_dict) & compare with #speeches in speeches_metadata (json)
+# 
+# Task 2:
+# speech_analysis.py: print cb_count_dict sorted by bank with most speeches (at the top): Example
+# Bank 1: 300
+# Bank 2: 125
+# Bank 3: 34
+
+# Task 3: Move code into function
+
+# Task 4:
+# speech_analysis.py: Take ouput of task 2 and align number of speeches
+# Bank 1: 300
+# Bank 2: 125
+# Bank 3:  55
+# Bank 4:   3
+
+# Task 5:
+# Calculate average and median of number of speeches
+
+# Task 6:
+# Compute weights of individual CBs
+
+# classes
+# understand what shell, terminal, batch are - which are important commands, rules of using a shell, use cases
+# how do search engines/text search work: Lemmatization/stemming/lexing/tokenization/word frequency analysis
+    # Search engine architecture and design
+# text files vs. binary files
+# understand ecosystem of different programming languages
+# refactoring my script with GPT and explain how to make functions smaller/flexible, i.e. how to refactor code into modules
+# understand match function in python
+# How to distinguish premature optimization from helpful optimization
+# Understand Git vs. Git gui etc.
+# context manager
+# how to build an iterator
+# typing of variables
+# with loop
+# while loops (closest to equivalent to do-while)
+# break 
+# continue
+# list comprehensions√
+# read up on file-handles in python documentation, e.g. why are PDFs binary files
